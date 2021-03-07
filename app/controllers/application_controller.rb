7c360571889347c7ef970ab2b6e6ea33b5434eb2
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-03-07 21:17:24 +0900

    ログインしているときは、トップページに「こんにちはユーザー名さん」が表示されるように条件分岐した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:profile, :occupation, :position])
+   devise_parameter_sanitizer.permit(:sign_up, 
+   keys: [:email, :password, :name, :profile, :occupation, :position])
+   devise_parameter_sanitizer.permit(:sign_in, 
+   keys: [:email, :password])
   end
 end
--- app/controllers/prototyps_controller.rb
+++
@@ -1,4 +0,0 @@
-class PrototypsController < ApplicationController
-  def index
-  end
-end

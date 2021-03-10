3f91285b7c681a2b8afbce85af526bffd24dd744
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-03-10 16:33:57 +0900

    アクションとルーティングを設定しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,18 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
   def index
-   
-
-
   end
+
+    def new
+      @prototypes = prototypes.new
+    end
+  
+    def create
+      prototypes.create(prototypes_params)
+    end
+  
+    private
+    def prototypes_params
+      params.require(:prototypes).permit(:name, :image, :text)
+    end 
 end

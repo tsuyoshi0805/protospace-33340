f0c4595f4323ffa92fd154bb6973ac391d623962
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-03-20 20:22:07 +0900

    トップページのプロトタイプをクリックすると、詳細ページに遷移するようにしましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,18 +1,36 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @prototypes = Prototype.all
   end
 
     def new
-      @prototypes = prototypes.new
+      @prototype = Prototype.new
     end
   
     def create
-      prototypes.create(prototypes_params)
+      @prototype = Prototype.new(prototypes_params)
+      
+        if @prototype.save
+          redirect_to root_path
+        else   
+          render :new
+        end
     end
+    def show
+      @prototype = Prototype.find(params[:id])
+    end
+
   
     private
     def prototypes_params
-      params.require(:prototypes).permit(:name, :image, :text)
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
     end 
-end
+    def move_to_index
+      unless user_signed_in?
+        redirect_to action: :index
+      end
+    end
+    
+
+  end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,12 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :prototypes, presence: true
+  validates :prototypes, presence: true, unless: :was_attached?
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
+  def was_attached?
+    self.image.attached?
+  end
+
 end

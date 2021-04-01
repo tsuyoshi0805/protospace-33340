c1d6145b7465f58907bf31bfa7f2fdbadd7cdd45
tsuyoshi0805 <76508504+tsuyoshi0805@users.noreply.github.com>
2021-04-01 22:03:14 +0900

    修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :destroy, :update]
-  before_action :move_to_index, except: [:index, :show, :update, :new]
+  before_action :move_to_index, except: [:index, :show, :update, :new, :create] 
   
 
 
